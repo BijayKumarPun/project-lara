e6daf7427209c53c917b1e83ddbc2d21bfb98a49
homay <bijay.workplace@gmail.com>
2020-04-24 17:38:28 +0545

    Make navbar functional

--- resources/views/main_layout.blade.php
+++ resources/views/main_layout.blade.php
@@ -1,19 +1,35 @@
 <html>
 <head>
 <title>BKP</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style relation="text/css">
+a{
+text-decoration:none;
+color:black;
+}
+li{
+list-style-type:none;
+display:inline;
+padding:10px;
+margin:10px;}
+ul{
+margin:20px;
+padding:20px;}
+.nav-bar{
+background-color:lightblue;
+}
+</style>
 </head>
 <body>
-<div class="navigation-bar">
+<div class="nav-bar" >
 <ul>
-<li>home</li>
-<li>blog</li>
-<li>about</li>
-</li>
+<li> <a href="/">home</a></li>
+<li> <a href="/blog">blog</a></li>
+<li> <a href="/about">about</a></li>
+</ul>
 </div>
-
-
-
-
+<div class="container">
 @yield('main_content')
+</div>
 </body>
 </html>
